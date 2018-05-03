6f3638b9b7ba2174ecd1f0b8753dbfdb958708d6
novotnaj <novotnaj@rose-hulman.edu>
2018-05-03 11:06:07 -0400

    yeet

--- src/m2_more_nested_loops_in_graphics.py
+++ src/m2_more_nested_loops_in_graphics.py
@@ -3,8 +3,8 @@ This project demonstrates NESTED LOOPS (i.e., loops within loops)
 in the context of TWO-DIMENSIONAL GRAPHICS.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Andrew Novotny.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -49,10 +49,20 @@ def draw_upside_down_wall(rectangle, n, window):
     and n is nonnegative.
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     Some tests are already written for you (above).
     # ------------------------------------------------------------------
+    og_rect = rectangle.clone()
 
+
+    for k in range(n):
+        for j in range(k + 1):
+            rectangle.attach_to(window)
+            rectangle = rectangle.clone()
+            rectangle.move_by(-rectangle.get_width(), 0)
+        window.render()
+        rectangle = og_rect.clone()
+        rectangle.move_by((rectangle.get_width() // 2) * (k + 1), -rectangle.get_height() * (k + 1))
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
